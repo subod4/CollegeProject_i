9648fc0fd0dd10b346679600cde39c310b16c4c3
Subodh Badal <subodhbadal111@gmail.com>
2024-01-22 20:08:41 +0545

    user fetch data from database

--- User/index.php
+++ User/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="style.css">-->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="../css/userstyle.css">
     <title>User Dashboard</title>
 </head>
 <body>
@@ -30,14 +30,26 @@
         }
         if (isset($_SESSION["user"])) 
         {
-            require_once "dbconnect.php";
             ?>
                 <div class="container">
                     <h1>Welcome to User Dashboard</h1>
                     <a href="logout.php" class="btn btn-warning">Logout</a>
-                    
-
                 </div>
+                <div class="product-container">
+        <?php
+            // Database connection 
+            require_once "dbconnect.php";
+            $select = mysqli_query($conn, "SELECT * FROM products");
+   
+            while($row = mysqli_fetch_assoc($select)) {
+        ?>
+            <div class="product-box">
+                <img src="../Uploads/<?php echo $row['productImage']; ?>" height="100" alt="">
+                <h3><?php echo $row['productName']; ?></h3>
+                <p>$<?php echo $row['productPrice']; ?>/-</p>
+            </div>
+        <?php } ?>
+    </div>
             <?php
         }
     ?>
